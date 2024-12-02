1694b5a2d46462998f030bd4b4f7ebc03ce9e0e3
aidaole <aidaole@outlook.com>
2024-12-02 20:47:23 +0800

    [first commit]

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'modules/home/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:ai_music/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
