5b32540bb75306cea32db94f352d14ba58bee458
aidaole <aidaole@outlook.com>
2024-12-04 23:47:56 +0800

    [添加用户信息缓存]

--- lib/modules/account/repositories/account_repository.dart
+++ lib/modules/account/repositories/account_repository.dart
@@ -1,15 +1,47 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 import '../../../common/log_util.dart';
 import '../../../network/dio_utils.dart';
 import '../models/account_model.dart';
 
 class AccountRepository {
+  static const String _cacheKey = 'account_cache';
+
   Future<AccountModel> getAccountInfo() async {
     try {
+      // 先尝试从缓存中获取数据
+      final cachedAccount = await _getCachedAccount();
+      if (cachedAccount != null) {
+        return cachedAccount;
+      }
+
+      // 如果缓存中没有数据，则从网络获取
       final resp = await DioUtils.get(path: "/login/status");
-      return AccountModel.fromJson(resp);
+      final account = AccountModel.fromJson(resp);
+
+      // 将获取到的数据缓存起来
+      await _cacheAccount(resp);
+
+      return account;
     } catch (e) {
       LogUtil.e('获取账号信息失败: $e');
       throw Exception('Failed to fetch account info');
     }
+  }
+
+  Future<AccountModel?> _getCachedAccount() async {
+    final prefs = await SharedPreferences.getInstance();
+    final jsonString = prefs.getString(_cacheKey);
+    if (jsonString != null) {
+      final jsonMap = json.decode(jsonString);
+      return AccountModel.fromJson(jsonMap);
+    }
+    return null;
+  }
+
+  Future<void> _cacheAccount(Map<String, dynamic> json) async {
+    final prefs = await SharedPreferences.getInstance();
+    final jsonString = jsonEncode(json);
+    await prefs.setString(_cacheKey, jsonString);
   }
 } 
